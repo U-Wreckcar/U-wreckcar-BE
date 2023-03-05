40726363a3b19e980245186439b900421f376ead
Seong Hyeon Kim <105336416+rtg1014@users.noreply.github.com>
2023-03-05 23:49:55 +0900

    :sparkles: excell 파일로 추출하기 기능 추가

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 import { router as UserRouter } from './src/routes/userRouter.js';
 import { router as UTMRouter } from './src/routes/utmRouter.js';
+// import { exportDataToExcel } from './src/controllers/utm/exportDataToExcel.js';
 
 const app = express();
 
@@ -27,15 +28,26 @@ app.use(
 app.use(UserRouter);
 app.use(UTMRouter);
 
-app.get('/export/excell', async (req, res) => {
+app.get('/', async (req, res) => {
     try {
-        await exportDataToExcel();
-        res.send('엑셀파일 익스퍼트가 잘 되었습니다');
+        res.send('Hi !!');
     } catch (error) {
         console.error(error);
-        res.status(500).send('엑셀파일 익스퍼트가 실패하였습니다.');
+        res.status(500).send('노 하이');
     }
 });
+
+
+// app.get('/export/excell', async (req, res) => {
+//     try {
+//         console.log("url 시작")
+//         exportDataToExcel();
+//         res.send('엑셀파일 익스퍼트가 잘 되었습니다');
+//     } catch (error) {
+//         console.error(error);
+//         res.status(500).send('엑셀파일 익스퍼트가 실패하였습니다.');
+//     }
+// });
 
 app.listen(process.env.SERVER_PORT, () =>
     console.log(`Server listening on ${process.env.SERVER_PORT}`)
--- src/routes/utmRouter.js
+++ src/routes/utmRouter.js
@@ -1,14 +1,22 @@
 import express from 'express';
+import { asyncWrapper } from '../../utils/middleware.js';
+import {exportDataToExcel} from '../controllers/utm/exportDataToExcel.js';
+
+
 const router = express.Router();
 
 // UTM 관련
-router.get('/api/utm', () => {});
-router.get('/api/utm', () => {});
-router.delete('/api/utm', () => {});
-router.post('/api/utm', () => {});
-router.post('/api/utm', () => {});
-router.post('/api/utm', () => {});
-router.post('/api/utm', () => {});
-router.post('/api/utm', () => {});
+router.get('/api/utm', () => { });
+router.get('/api/utm', () => { });
+router.delete('/api/utm', () => { });
+router.post('/api/utm', () => { });
+router.post('/api/utm', () => { });
+router.post('/api/utm', () => { });
+router.post('/api/utm', () => { });
+router.post('/api/utm', () => { });
+
+// UTM 데이터 추출 관련
+router.get('/export/excell', asyncWrapper(exportDataToExcel));
+
 
 export { router };
