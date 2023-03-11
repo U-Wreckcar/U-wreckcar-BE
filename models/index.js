0808636eb02733d3a694ae364d7b47bf1d8aed51
tastekim <tastekim@kakao.com>
2023-03-12 03:48:21 +0900

    :bug: sequelize migrate 에러 수정 / dependencies 정리

--- models/index.js
+++ models/index.js
@@ -1,45 +1,33 @@
+import dotenv from 'dotenv';
 import fs from 'fs';
 import path from 'path';
-import Sequelize from 'sequelize';
-import process from 'process';
+import { Sequelize } from 'sequelize';
 
-const basename = path.basename(new URL(import.meta.url).pathname);
+dotenv.config();
+
+const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = import('../config/config.js')[env];
+const config = require('/../config/config.js')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+const sequelize = new Sequelize(config);
 
-fs
-    .readdirSync(path.dirname(new URL(import.meta.url).pathname))
-    .filter(file => {
-      return (
-          file.indexOf('.') !== 0 &&
-          file !== basename &&
-          file.slice(-3) === '.js' &&
-          file.indexOf('.test.js') === -1
-      );
+fs.readdirSync(__dirname)
+    .filter((file) => {
+        return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
     })
-    .forEach(file => {
-      const model = import(path.join(path.dirname(new URL(import.meta.url).pathname), file)).default;
-      db[model.name] = model(sequelize, Sequelize.DataTypes);
+    .forEach((file) => {
+        const model = require(path.join(__dirname, file)).default(sequelize, Sequelize.DataTypes);
+        db[model.name] = model;
     });
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+Object.keys(db).forEach((modelName) => {
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
 
-sequelize.sync({ force: true });
-
-// force true 시 디비 날려버리고 새로만듭니다! 첫실행 후 꼭 false로 바꿔주기!
-
-export { sequelize, Sequelize };
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
 
 export default db;
