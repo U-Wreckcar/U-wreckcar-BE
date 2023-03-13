a5d22856e623d04e933454c22b609f9d465e4abf
Seong Hyeon Kim <105336416+rtg1014@users.noreply.github.com>
2023-03-13 21:55:36 +0900

    FEAT : :sparkles: :recycle: UTM 추출하기를 위한 필터링 함수추가 / UTM 메모 수정하기능 추가

--- src/routes/utmRouter.js
+++ src/routes/utmRouter.js
@@ -1,22 +1,23 @@
 import express from 'express';
 import { asyncWrapper } from '../../utils/middleware.js';
-import {exportDataToExcel} from '../controllers/utm/exportDataToExcel.js';
-
-
+import { exportDataToExcel } from '../controllers/utm/exportDataToExcel.js';
+import { utmFilters } from '../controllers/utm/utmFilter.js';
+import { utmMemo } from '../controllers/utm/utmMemo.js';
 const router = express.Router();
 
 // UTM 관련
-router.get('/api/utm', () => { });
-router.get('/api/utm', () => { });
-router.delete('/api/utm', () => { });
-router.post('/api/utm', () => { });
-router.post('/api/utm', () => { });
-router.post('/api/utm', () => { });
-router.post('/api/utm', () => { });
-router.post('/api/utm', () => { });
+router.get('/api/utm', () => {});
+router.get('/api/utm', () => {});
+router.delete('/api/utm', () => {});
+router.post('/api/utm', () => {});
+router.post('/api/utm', () => {});
+router.post('/api/utm', () => {});
+router.post('/api/utm', () => {});
+router.post('/api/utm', () => {});
 
 // UTM 데이터 추출 관련
-router.get('/export/excell', asyncWrapper(exportDataToExcel));
-
+router.post('/api/utms/excell', asyncWrapper(exportDataToExcel));
+router.post('/api/utms/filter', asyncWrapper(utmFilters));
+router.patch('/api/utm/memo', asyncWrapper(utmMemo));
 
 export { router };
