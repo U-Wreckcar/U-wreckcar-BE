bc030f5e170692330dbc4c3b174505312d8984f1
tastekim <tastekim@kakao.com>
2023-03-19 06:04:18 +0900

    :recycle: createUtm 배열로 받아서 생성.

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -8,8 +8,8 @@ import { alreadyExists } from '../modules/user.module.js';
 const router = express.Router();
 
 // kakao 로그인
-router.get('/auth/kakao', kakaoLogin);
-router.get('/auth/kakao/callback', kakaoCallback, async (req, res) => {
+router.get('/api/auth/kakao', kakaoLogin);
+router.get('/api/auth/kakao/callback', kakaoCallback, async (req, res) => {
     try {
         const { access_token, refresh_token, user } = req.user;
         console.log('AccessToken : ', access_token);
@@ -17,9 +17,7 @@ router.get('/auth/kakao/callback', kakaoCallback, async (req, res) => {
 
         // 기존 회원 확인 후 새로 가입.
         await alreadyExists(user);
-        res.cookie('access_token', access_token, { sameSite: 'none', secure: true });
-        res.cookie('refresh_token', refresh_token, { sameSite: 'none', secure: true });
-        res.status(201);
+        res.status(200).send({ access_token: access_token, refresh_token: refresh_token });
     } catch (err) {
         console.error(err);
         res.status(500).send({ errorMessage: err.message, stack: err.stack });
