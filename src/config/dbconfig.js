fedb6824af384a731c1ffb1368944ff9142f29b6
Seong Hyeon Kim <105336416+rtg1014@users.noreply.github.com>
2023-03-20 17:34:30 +0900

    FIX : DB 커넥션 부분 오류 변경

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -4,9 +4,9 @@ dotenv.config();
 
 export default {
     test_db_config: {
-        host: process.env.MYSQL_DB_ENV_HOST,
-        user: process.env.MYSQL_DB_ENV_USER,
-        password: process.env.MYSQL_DB_ENV_PASSWORDM,
-        database: process.env.MYSQL_DB_ENV_DATABASE,
+        host: process.env.RDS_HOST,
+        user: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DBNAME,
     },
 };
