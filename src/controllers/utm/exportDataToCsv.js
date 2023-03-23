26b7c6cb3d13a4d979b44fb3754590dc83e34731
Seong Hyeon Kim <105336416+rtg1014@users.noreply.github.com>
2023-03-23 18:24:10 +0900

    FIX : :art: 추출하기 메소드를 GET 요청으로 변경, 쿼리스트링으로 id 값 받음

--- src/controllers/utm/exportDataToCsv.js
+++ src/controllers/utm/exportDataToCsv.js
@@ -27,38 +27,53 @@ async function exportDataToCsv(req, res) {
     // }
     // deleteAllFilesInFolder(delete_path);
 
+    const input = req.query.utm_id;
+    console.log('쿼리값이 뭔지 알려줌', input);
+
+    let sql_id_add = 'WHERE ';
+    let id_stack = 1;
+    input.forEach((id) => {
+        id_stack--;
+        if (id_stack < 0) {
+            sql_id_add = sql_id_add + 'or ';
+        }
+        sql_id_add = sql_id_add + `utm.utm_id = '${id}' `;
+    });
+
     const db = config.test_db_config;
     const connection = await createConnection(db);
 
     // const input = req.body;
     // console.log(input);
-    const input = [
-        {
-            utm_id: 62,
-            utm_url: 'naver.com/',
-            utm_campaign_id: 'blog',
-            utm_campaign_name: 'blogproject',
-        },
-        {
-            utm_id: 63,
-            utm_url: 'naver.com/',
-            utm_campaign_id: 'blog',
-            utm_campaign_name: 'blogproject',
-        },
-        {
-            utm_id: 64,
-            utm_url: 'naver.com/',
-            utm_campaign_id: 'blog',
-            utm_campaign_name: 'blogproject',
-        },
-        {
-            utm_id: 83,
-            utm_url: 'naver.com/',
-            utm_campaign_id: 'blog',
-            utm_campaign_name: 'blogproject',
-        },
-    ];
+    // const input = [
+    //     {
+    //         utm_id: 62,
+    //         utm_url: 'naver.com/',
+    //         utm_campaign_id: 'blog',
+    //         utm_campaign_name: 'blogproject',
+    //     },
+    //     {
+    //         utm_id: 63,
+    //         utm_url: 'naver.com/',
+    //         utm_campaign_id: 'blog',
+    //         utm_campaign_name: 'blogproject',
+    //     },
+    //     {
+    //         utm_id: 64,
+    //         utm_url: 'naver.com/',
+    //         utm_campaign_id: 'blog',
+    //         utm_campaign_name: 'blogproject',
+    //     },
+    //     {
+    //         utm_id: 83,
+    //         utm_url: 'naver.com/',
+    //         utm_campaign_id: 'blog',
+    //         utm_campaign_name: 'blogproject',
+    //     },
+    // ];
 
+    /*
+    여기는 post 요청시 실행 되는 로직
     const utm_id_arr = [];
     input.forEach((index) => {
         const utm_id = index['utm_id'];
@@ -75,6 +90,9 @@ async function exportDataToCsv(req, res) {
         }
         sql_id_add = sql_id_add + `utm.utm_id = '${id}' `;
     });
+
+    */
+
     const sql_query = `SELECT  DATE_FORMAT(utm.created_at, '%Y-%m-%d') as 생성일자, utm.utm_url as URL ,utm.utm_campaign_id as 캠페인ID,utm.utm_url, source.source_name as 소스, medium.medium_name as 미디움, utm.utm_campaign_name as 캠페인이름, utm.utm_term as 캠페인_텀, utm.utm_content as 캠페인콘텐츠, utm.full_url as UTM, utm.shorten_url as Shorten_URL, utm.utm_memo as 메모 FROM uwreckcar_db.Utms as utm LEFT join User_utm_sources as source on utm.user_utm_source_id = source.user_utm_source_id JOIN User_utm_mediums as medium on utm.user_utm_medium_id = medium.user_utm_medium_id ${sql_id_add} group by utm.utm_id `;
     console.log('실행될 쿼리문', sql_query);
 
