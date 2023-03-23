42198f1523b414ca6bb76c80538d636cc94594e3
Seong Hyeon Kim <105336416+rtg1014@users.noreply.github.com>
2023-03-24 06:54:49 +0900

    FIX : GET 요청으로 하고 body 값 전달

--- src/controllers/utm/exportDataToCsv.js
+++ src/controllers/utm/exportDataToCsv.js
@@ -8,130 +8,74 @@ import os from 'os';
 import config from '../../config/dbconfig.js';
 import contentDisposition from 'content-disposition';
 import { file_download } from './fileDownload.js';
+import { async } from 'regenerator-runtime';
 const fsPromises = fs.promises;
 
 async function exportDataToCsv(req, res) {
-    //     // 파일 삭제 로직 생성
-    // const delete_path = `src\\controllers\\utm\\export\\`;
-    // // const delete_path = `src/controllers/utm/export/`;
-    // async function deleteAllFilesInFolder(folderPath) {
-    //     fs.readdir(folderPath, (err, files) => {
-    //         if (err) throw err;
-    //         for (const file of files) {
-    //             fs.unlink(path.join(folderPath, file), (err) => {
-    //                 if (err) throw err;
-    //                 console.log('파일삭제완료')
-    //             });
-    //         }
-    //     });
-    // }
-    // deleteAllFilesInFolder(delete_path);
+    try {
+        console.log(11111111);
+        const db = config.test_db_config;
+        const connection = await createConnection(db);
 
-    const input = req.query.utm_id;
-    console.log('쿼리값이 뭔지 알려줌', input);
+        const input = req.body;
+        console.log(input);
 
-    let sql_id_add = 'WHERE ';
-    let id_stack = 1;
-    input.forEach((id) => {
-        id_stack--;
-        if (id_stack < 0) {
-            sql_id_add = sql_id_add + 'or ';
-        }
-        sql_id_add = sql_id_add + `utm.utm_id = '${id}' `;
-    });
+        let sql_id_add = 'WHERE ';
+        let id_stack = 1;
+        input.forEach((id) => {
+            id_stack--;
+            if (id_stack < 0) {
+                sql_id_add = sql_id_add + 'or ';
+            }
+            sql_id_add = sql_id_add + `utm.utm_id = '${id}' `;
+        });
+        console.log('sql 아이디 웨얼문 ㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ', sql_id_add);
+        const sql_query = `SELECT  DATE_FORMAT(utm.created_at, '%Y-%m-%d') as 생성일자, utm.utm_url as URL ,utm.utm_campaign_id as 캠페인ID,utm.utm_url, source.source_name as 소스, medium.medium_name as 미디움, utm.utm_campaign_name as 캠페인이름, utm.utm_term as 캠페인_텀, utm.utm_content as 캠페인콘텐츠, utm.full_url as UTM, utm.shorten_url as Shorten_URL, utm.utm_memo as 메모 FROM uwreckcar_db.Utms as utm LEFT join User_utm_sources as source on utm.user_utm_source_id = source.user_utm_source_id JOIN User_utm_mediums as medium on utm.user_utm_medium_id = medium.user_utm_medium_id ${sql_id_add} group by utm.utm_id `;
 
-    const db = config.test_db_config;
-    const connection = await createConnection(db);
+        // const query3 = query;
+        const filename = 'UTM_Data_File_CSV.csv';
+        const filepath = `src\\controllers\\utm\\export\\${filename}`;
+        // const filepath = `src\\controllers\\utm\\export\\${filename}`;
 
-    // const input = req.body;
-    // console.log(input);
-    // const input = [
-    //     {
-    //         utm_id: 62,
-    //         utm_url: 'naver.com/',
-    //         utm_campaign_id: 'blog',
-    //         utm_campaign_name: 'blogproject',
-    //     },
-    //     {
-    //         utm_id: 63,
-    //         utm_url: 'naver.com/',
-    //         utm_campaign_id: 'blog',
-    //         utm_campaign_name: 'blogproject',
-    //     },
-    //     {
-    //         utm_id: 64,
-    //         utm_url: 'naver.com/',
-    //         utm_campaign_id: 'blog',
-    //         utm_campaign_name: 'blogproject',
-    //     },
-    //     {
-    //         utm_id: 83,
-    //         utm_url: 'naver.com/',
-    //         utm_campaign_id: 'blog',
-    //         utm_campaign_name: 'blogproject',
-    //     },
-    // ];
+        const test1 = 'SELECT * FROM uwreckcar_db.Utms';
+        const test2 = 'SELECT * FROM uwreckcar_db.Utms where utm_id=143';
+        //-------------------
 
-    /*
-    여기는 post 요청시 실행 되는 로직
-    const utm_id_arr = [];
-    input.forEach((index) => {
-        const utm_id = index['utm_id'];
-        utm_id_arr.push(utm_id);
-    });
-    console.log(utm_id_arr);
+        // create a connection to your MySQL databas
 
-    let sql_id_add = 'WHERE ';
-    let id_stack = 1;
-    utm_id_arr.forEach((id) => {
-        id_stack--;
-        if (id_stack < 0) {
-            sql_id_add = sql_id_add + 'or ';
-        }
-        sql_id_add = sql_id_add + `utm.utm_id = '${id}' `;
-    });
+        // your SQL query
 
-    */
+        // run the query
+        // const query2 = 'SELECT * FROM uwreckcar_db.Utms';
+        const [rows] = await connection.execute(sql_query);
+        // console.log('여기는 로우스 입니달ㄹㄹㄹㄹㄹㄹㄹㄹㄹㄹ', [rows]);
 
-    const sql_query = `SELECT  DATE_FORMAT(utm.created_at, '%Y-%m-%d') as 생성일자, utm.utm_url as URL ,utm.utm_campaign_id as 캠페인ID,utm.utm_url, source.source_name as 소스, medium.medium_name as 미디움, utm.utm_campaign_name as 캠페인이름, utm.utm_term as 캠페인_텀, utm.utm_content as 캠페인콘텐츠, utm.full_url as UTM, utm.shorten_url as Shorten_URL, utm.utm_memo as 메모 FROM uwreckcar_db.Utms as utm LEFT join User_utm_sources as source on utm.user_utm_source_id = source.user_utm_source_id JOIN User_utm_mediums as medium on utm.user_utm_medium_id = medium.user_utm_medium_id ${sql_id_add} group by utm.utm_id `;
-    console.log('실행될 쿼리문', sql_query);
+        const csvStream = csv.format({ headers: true, encoding: 'utf8mb4' });
 
-    // 파일경로 설정
-    const filename = 'UTM_Data_File_CSV.csv';
-    const filepath = `src\\controllers\\utm\\export\\${filename}`;
+        csvStream.pipe(fs.createWriteStream(filepath, { encoding: 'utf8' })).on('finish', () => {
+            console.log('CSV 파일이 잘 완성되었습니다.');
+        });
 
-    // 쿼리 실행문 rows 에 담은 후 csv 파일 생성후 입력
-    const [rows] = await connection.execute(sql_query);
+        rows.forEach((row) => {
+            csvStream.write(row);
+        });
+        // close the MySQL connection
+        connection.end();
+        //----------------------
+        console.log(22222222);
+        // // 추출하기 추가
+    } catch (err) {
+        console.log(err);
+        console.log(33333333);
+    } finally {
+        console.log(4444444);
+        const filename = 'UTM_Data_File_CSV.csv';
+        const filepath = `src\\controllers\\utm\\export\\${filename}`;
+        //파일 formater 지정
+        let contentType = 'text/csv';
+        console.log(contentType);
 
-    const csvStream = csv.format({ headers: true, encoding: 'utf8mb4' });
-    csvStream.pipe(fs.createWriteStream(filepath, { encoding: 'utf8' })).on('finish', () => {
-        console.log('CSV 파일이 잘 완성되었습니다.');
-    });
-    rows.forEach((row) => {
-        csvStream.write(row);
-    });
 
-    // 커넥션 종료
-    connection.end();
-
-    // 추출하기 로직
-    //파일 formater 지정
-    let contentType = '';
-    if (filename.endsWith('.xlsx')) {
-        contentType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
-    } else if (filename.endsWith('.csv')) {
-        contentType = 'text/csv';
-    } else if (filename.endsWith('.pdf')) {
-        contentType = 'application/pdf';
-    } else {
-        return res.status(400).send('지원하지 않는 파일 포멧 입니다.');
-    }
-    console.log(contentType);
-
-    // 사용자의 브라우저가 파일을 다운로드하도록 요청하는 데 필요한 헤더 설정
-    // 파일 추출후 읽을 수 있는 상태로 추출하기를 할 수 있도록 포멧터 설정
-    // 파일 세팅
-    function export_file_user() {
         fs.stat(filepath, (err, stat) => {
             if (err) {
                 console.error(err);
@@ -139,16 +83,17 @@ async function exportDataToCsv(req, res) {
             }
             res.setHeader('Content-Type', contentType);
             res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
-            res.setHeader('Content-Transfer-Encoding', 'binary');
-            res.setHeader('Cache-Control', 'public, max-age=0');
-            res.setHeader('Expires', '-1');
-            res.setHeader('Pragma', 'public');
-            res.setHeader('Content-Length', stat.size);
-            res.setHeader('compress', false);
-
-            // 파일읽은후 내보내기 최종
+            // res.setHeader('Content-Transfer-Encoding', 'binary');
+            // res.setHeader('Cache-Control', 'public, max-age=0');
+            // res.setHeader('Expires', '-1');
+            // res.setHeader('Pragma', 'public');
+            // res.setHeader('Content-Length', stat.size);
+            // res.setHeader('compress', false);
+            console.log('여기는 파일스트림 직전');
             const fileStream = fs.createReadStream(filepath);
-            fileStream.pipe(res);
+            if (fileStream) {
+                fileStream.pipe(res);
+            }
             fileStream.on('close', () => {
                 console.log('File stream closed');
             });
@@ -157,25 +102,9 @@ async function exportDataToCsv(req, res) {
             });
         });
     }
-
-    const myFunction = async () => {
-        try {
-            await new Promise((resolve, reject) => {
-                export_file_user((err, result) => {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(result);
-                    }
-                });
-            });
-            console.log('콜백함수가 무사히 실행되었습니다.');
-        } catch (err) {
-            // Error handling code
-            console.error('에러가 발생했습니다.', err);
-        }
-    };
-    myFunction();
 }
+// 파일 세팅
+
+// exportDataToCsv.then()
 
 export { exportDataToCsv };
