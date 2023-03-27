46f9d7122b63466aae5099167aec6f6446c44403
tastekim <tastekim@kakao.com>
2023-03-27 22:27:24 +0900

    :page_facing_up: nodemailer 양식 수정

--- src/controllers/user/companySignup.js
+++ src/controllers/user/companySignup.js
@@ -119,11 +119,12 @@ export async function sendEmailController(req, res, next) {
         <img src="https://velog.velcdn.com/images/tastekim_/post/035ebb31-ff86-495e-a7a6-65ba041e9318/image.jpg" alt="Service Image" style="max-width: 100%; height: auto;">
     </div>
     <h1 style="font-weight: normal; margin-bottom: 0.5rem; color: #222;">유렉카 서비스를 이용해주셔서 감사합니다!</h1>
-    <p style="margin-bottom: 1rem; color: #444; font-size: 18px; line-height: 1.4;">Dear [Name],</p>
-    <p style="margin-bottom: 1rem; color: #444; font-size: 18px; line-height: 1.4;">We are excited to welcome you to our mailing list! You will now receive our latest news, promotions, and updates. We promise to keep your inbox interesting and not spammy.</p>
+    <p style="margin-bottom: 1rem; color: #444; font-size: 18px; line-height: 1.4;">${email.split('@')[0]} 님,</p>
+    <p style="margin-bottom: 1rem; color: #444; font-size: 18px; line-height: 1.4;">아래 인증 코드를 가지고 이메일 인증을 완료해주세요.</p>
+     <p style="margin-bottom: 1rem; color: #444; font-size: 18px; line-height: 1.4;">* 한 번 사용된 메일은 재사용 할 수 없습니다. 인증을 받으신 후 메일을 삭제해주세요.</p>
     <p style="margin-bottom: 1rem; color: #444; font-size: 18px; line-height: 1.4;">인증 코드 : ${verificationCode}<a href="#" style="color: #0070c0; text-decoration: underline; margin-left: 0.5rem;">contact us</a>.</p>
     <a href="https://utm.works" class="button" style="display: inline-block; background-color: #0070c0; color: #fff; padding: 10px 20px; text-decoration: none; border-radius: 5px; margin-top: 1rem;">Explore our website</a>
-    <p class="footer" style="margin-top: 2rem; text-align: center; color: #888;">Best regards,<br>The [Your Company] Team<br> © 2023 [Your Company] All rights reserved.</p>
+    <p class="footer" style="margin-top: 2rem; text-align: center; color: #888;">Best regards,<br>Team 유렉카<br> © 2023 유렉카 All rights reserved.</p>
 </div>
 </body>
 </html>
