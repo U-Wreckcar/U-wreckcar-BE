093780c5186dd7c53255df9bc7c4f330444ab1c0
tastekim <tastekim@kakao.com>
2023-04-02 03:41:37 +0900

    :recycle: 로그인 방식 통합(토큰 통합) 6

--- src/passport/index.js
+++ src/passport/index.js
@@ -1,16 +1,16 @@
-import { serializeUser, deserializeUser } from 'passport';
-// import local from './localStrategy'; // 로컬서버로 로그인할때
-import google from './googleStrategy'; // 구글서버로 로그인할때
-import Users from '../../models/users';
-export default () => {
-    serializeUser((user, done) => {
-        done(null, user.id);
-    });
-
-    deserializeUser((id, done) => {
-        Users.findOne({ where: { id } })
-            .then((Users) => done(null, Users))
-            .catch((err) => done(err));
-    });
-    google(); // 구글 전략 등록
-};
+// import { serializeUser, deserializeUser } from 'passport';
+// // import local from './localStrategy'; // 로컬서버로 로그인할때
+// import google from './googleStrategy'; // 구글서버로 로그인할때
+// import Users from '../../models/users';
+// export default () => {
+//     serializeUser((user, done) => {
+//         done(null, user.id);
+//     });
+//
+//     deserializeUser((id, done) => {
+//         Users.findOne({ where: { id } })
+//             .then((Users) => done(null, Users))
+//             .catch((err) => done(err));
+//     });
+//     google(); // 구글 전략 등록
+// };
