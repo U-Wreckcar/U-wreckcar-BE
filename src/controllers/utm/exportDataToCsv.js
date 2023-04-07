a15a42e10bf8fd0fb5025218dec538363f4ac2fe
Seong Hyeon Kim <105336416+rtg1014@users.noreply.github.com>
2023-04-07 22:15:41 +0900

    :wrench: FIX : 대용량 업로드 양식을 위해 key값으로 사용될 값으로 쿼리수정

--- src/controllers/utm/exportDataToCsv.js
+++ src/controllers/utm/exportDataToCsv.js
@@ -27,7 +27,7 @@ async function exportDataToCsv(req, res) {
         }
         sql_id_add = sql_id_add + `utm.utm_id = '${id}' `;
     });
-    const sql_query = `SELECT  DATE_FORMAT(utm.created_at, '%Y-%m-%d') as 생성일자, utm.utm_url as URL ,utm.utm_campaign_id as 캠페인ID,utm.utm_url, source.source_name as 소스, medium.medium_name as 미디움, utm.utm_campaign_name as 캠페인이름, utm.utm_term as 캠페인_텀, utm.utm_content as 캠페인콘텐츠, utm.full_url as UTM, utm.shorten_url as Shorten_URL, utm.utm_memo as 메모 FROM uwreckcar_db.Utms as utm LEFT join User_utm_sources as source on utm.user_utm_source_id = source.user_utm_source_id JOIN User_utm_mediums as medium on utm.user_utm_medium_id = medium.user_utm_medium_id ${sql_id_add} group by utm.utm_id `;
+    const sql_query = `SELECT  DATE_FORMAT(utm.created_at, '%Y-%m-%d') as created_at, utm.utm_url as URL ,utm.utm_campaign_id as 캠페인ID,utm.utm_url, source.source_name as 소스, medium.medium_name as 미디움, utm.utm_campaign_name as 캠페인이름, utm.utm_term as 캠페인_텀, utm.utm_content as 캠페인콘텐츠, utm.full_url as full_url, utm.shorten_url as Shorten_URL, utm.utm_memo as utm_memo FROM uwreckcar_db.Utms as utm LEFT join User_utm_sources as source on utm.user_utm_source_id = source.user_utm_source_id JOIN User_utm_mediums as medium on utm.user_utm_medium_id = medium.user_utm_medium_id ${sql_id_add} group by utm.utm_id `;
 
     const filename = 'UTM_Data_File_CSV';
     const filepath = path.join(__dirname, 'export', 'UTM_Data_File_CSV');
